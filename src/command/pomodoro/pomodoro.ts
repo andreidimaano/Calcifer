aa6a01d05c8c5ce1d9259469bd21cee40696d725
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-22 17:22:48 -0800

    refactor message.author.tag

--- src/command/pomodoro/pomodoro.ts
+++ src/command/pomodoro/pomodoro.ts
@@ -2,6 +2,7 @@ import { Message } from "discord.js";
 import firebase from "firebase";
 import { firebaseApp } from "../../database/FirebaseConfig";
 import { addUser, updateUserTotalTime } from "../../database/FirebaseCRUD";
+import { canceledPomodoroMembers, isCanceledPomodoro } from "../cancel/PomodoroCanceledMembers";
 import { endEmbed, startEmbed } from "./PomodoroEmbed";
 import { currentlyWorking, currentMembersWorking, removeMember} from "./PomodoroMembers";
 
@@ -10,33 +11,36 @@ let userDatabase = firebase.firestore();
 
 export let Pomodoro = async ( message: Message, time?: number) => {
     let timer = (time && time <= 120 && time >= 10) ? time : 25
+    let author = message.author.tag;
     
     //check if currentlyStudying
-    if(currentlyWorking(message.author.tag)) {
+    if(currentlyWorking(author)) {
         return await message.reply('You\'re already working!');
     }
     
     //if not currentlyStudying start Pomodoro
     //(1) update currentMembersStudying
-    currentMembersWorking.push(message.author.tag);
+    currentMembersWorking.push(author);
     //(2) send confirmation timer
     let errorMessage = (time && (time > 120 || time < 10)) ? 'timer specified is not within time limits, timer set to 25\n' : '';     
     await message.reply(errorMessage, startEmbed(timer));
 
     setTimeout(async () => {
+        if(isCanceledPomodoro(author))
+
         await message.channel.send(message.author, endEmbed);
-        removeMember(currentMembersWorking, message.author.tag);
+        removeMember(currentMembersWorking, author);
         
         //update database
-        updateDatabase(message, timer);
+        updateDatabase(author, timer);
         
     }, /*60000*/ 1000 * timer);   
 }
 
-let updateDatabase = async (message: Message, timer: Number) => {
+let updateDatabase = async (author: string, timer: Number) => {
     //if user exists in DB, execute Update operation; execute Add operation otherwise.
-    let userDB = await userDatabase.collection('Guild').doc(message.author.tag).get();
-    (userDB) ? await updateUserTotalTime(userDatabase, message.author.tag, userDB, timer) : await addUser(userDatabase, message.author.tag, timer);
+    let userDB = await userDatabase.collection('Guild').doc(author).get();
+    (userDB) ? await updateUserTotalTime(userDatabase, author, userDB, timer) : await addUser(userDatabase, author, timer);
 }
 
 

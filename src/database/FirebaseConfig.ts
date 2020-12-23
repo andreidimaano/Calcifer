cf654154498d68a2fe1ce83826a3e7f9d1a04ae1
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-22 20:37:20 -0800

    finish refactoring

--- src/database/FirebaseConfig.ts
+++ src/database/FirebaseConfig.ts
@@ -1,8 +1,7 @@
 require('dotenv').config();
 import firebase from 'firebase';
-// import firebase from 'firebase'
 import 'firebase/firestore';
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 export const firebaseConfig = {
     apiKey: process.env.FIREBASE_APIKEY,
     authDomain: process.env.FIREBASE_AUTHDOMAIN,

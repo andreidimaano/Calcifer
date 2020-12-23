1daafa9474fdae85177e60940223a0216abf78a2
Andrei Dimaano <andreiidimaano@gmail.com>
2020-12-22 20:26:26 -0800

    fix grammar issues

--- src/command/productivity/productivity.ts
+++ src/command/productivity/productivity.ts
@@ -9,19 +9,20 @@ export let Productivity = async (message: Message) : Promise<void> => {
     if(minutesStudied === -1){
         await message.reply(productivityMessage(minutesStudied.toString()));
     } else {
-        minutesToHours(minutesStudied);
-        await message.reply(productivityMessage(minutesStudied.toString()));
+        let messageConversion = minutesToHours(minutesStudied);
+        await message.reply(productivityMessage(messageConversion));
     }
     
 }
 
 let productivityMessage = (minutesStudied: string): string => {
-    return `\`\`\`\`Total Focused Time: ${minutesStudied}\`\`\`\``;
+    return `\`\`\`Total Focused Time: ${minutesStudied}\`\`\``;
 }
 
 let minutesToHours = (totalMinutes: number): string => {
-    var hours = Math.floor(totalMinutes / 60);          
-    var minutes = totalMinutes % 60;
+    let hours = Math.floor(totalMinutes / 60);          
+    let minutes = totalMinutes % 60;
+    let hourSingularOrPlural = (hours === 1) ? 'hour' : 'hours';
 
-    return (hours === 0) ? `${minutes} minutes`: `${hours} hours ${minutes} minutes`;
+    return (hours === 0) ? `${minutes} minutes`: `${hours} ${hourSingularOrPlural} ${minutes} minutes`;
 }

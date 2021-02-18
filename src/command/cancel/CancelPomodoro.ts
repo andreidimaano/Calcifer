1ffdb9dd2ed41a8d25647068d66fde15ed75c81d
Andrei Dimaano <andreiidimaano@gmail.com>
2021-02-18 15:46:04 -0800

    Hot Fix c: cancel

--- src/command/cancel/CancelPomodoro.ts
+++ src/command/cancel/CancelPomodoro.ts
@@ -1,24 +1,32 @@
 import { Message } from "discord.js";
+import { deleteUserOnBreak, isOnBreak } from "../../database/resolvers/UserOnBreakResolver";
+import { deleteUserWorking, isWorking } from "../../database/resolvers/UserStudyingResolver";
 import { printArray, removeMember } from "../pomodoro/ArrayFunctions";
 import { currentlyOnBreak, currentMembersOnBreak } from "../pomodoro/BreakMembers";
 import { currentlyWorking, currentMembersWorking } from "../pomodoro/PomodoroMembers";
 import { canceledBreakMembers, canceledPomodoroMembers } from "./PomodoroCanceledMembers";
 
 export let CancelPomodoro = async (message: Message) : Promise<void> => {
-    if(currentlyOnBreak(message.author.tag)) {
-        removeMember(currentMembersOnBreak, message.author.tag);
+    let currentlyWorking = await isWorking(message.author.id);
+    let currentlyOnBreak = await isOnBreak(message.author.id);
+
+
+    if(currentlyOnBreak) {
+        await deleteUserOnBreak(message.author.id);
+        // removeMember(currentMembersOnBreak, message.author.tag);
         canceledBreakMembers.push(message.author.tag);
         await message.reply(
             'Break Canceled'
         );
         return;
-    } else if(!currentlyWorking(message.author.tag) && !currentlyOnBreak(message.author.tag)) {
+    } else if(!currentlyWorking && !currentlyOnBreak) {
         await message.reply(
             'You are not currently working and not currently on break! Start a session by typing ```c: pomodoro```'
         );
         return;
-    } else if(currentlyWorking(message.author.tag)){
-        removeMember(currentMembersWorking, message.author.tag);
+    } else if(currentlyWorking){
+        await deleteUserWorking(message.author.id);
+        // removeMember(currentMembersWorking, message.author.tag);
         canceledPomodoroMembers.push(message.author.tag);
         await message.reply(
             'Pomodoro Canceled'
